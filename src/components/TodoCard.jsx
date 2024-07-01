b5c4dcff054f1d5ebc4a55f5e16f5508285ff4f5
Hancer <hancermercedes@gmail.com>
2024-07-01 13:43:41 -0400

    adding the completed todo button

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 export const TodoCard = (props) => {
   const { children, handleDeleteTodo, handleEditTodo, index } = props;
+  const [complete, setComplete] = useState(false);
   return (
-    <li className="todoItem">
+    <li className={`${complete ? "completed" : ""} todoItem`}>
       {children}
       <div className="actionsContainer">
+        <button
+          onClick={() => {
+            setComplete(true);
+          }}
+        >
+          <i className="fa-solid fa-circle-check"></i>
+        </button>
         <button
           onClick={() => {
             handleEditTodo(index);
@@ -11,13 +21,13 @@ export const TodoCard = (props) => {
         >
           <i className="fa-regular fa-pen-to-square"></i>
         </button>
-        <buttons
+        <button
           onClick={() => {
             handleDeleteTodo(index);
           }}
         >
           <i className="fa-solid fa-trash-can"></i>
-        </buttons>
+        </button>
       </div>
     </li>
   );
