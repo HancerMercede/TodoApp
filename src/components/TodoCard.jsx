1f093c140aa76879cbd4d06a51b812e00da2e45a
Hancer <hancermercedes@gmail.com>
2025-04-10 21:10:01 -0400

    Adding new features to the completed mark

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,15 +1,20 @@
-import { useState } from "react";
 import PropTypes from "prop-types";
 import "animate.css";
+import { useState } from "react";
 
 export const TodoCard = (props) => {
+  const [completed, setCompleted] = useState(false);
+
   const { children, handleDeleteTodo, handleEditTodo, index } = props;
-  const [complete, setComplete] = useState(false);
+
+  const handleCompleted = () => {
+    setCompleted(true);
+  };
 
   return (
     <li
       className={`${
-        complete ? "completed" : ""
+        completed ? "completed" : ""
       } todoItem animate__animated animate__fadeIn card`}
     >
       {children}
@@ -17,7 +22,7 @@ export const TodoCard = (props) => {
         <button
           id={"Check"}
           onClick={() => {
-            setComplete(true);
+            handleCompleted();
           }}
         >
           <i className="fa-solid fa-circle-check"></i>
@@ -47,5 +52,7 @@ TodoCard.propTypes = {
   children: PropTypes.element,
   handleDeleteTodo: PropTypes.func,
   handleEditTodo: PropTypes.func,
+  handleComplete: PropTypes.func,
   index: PropTypes.number,
+  completed: PropTypes.bool,
 };
